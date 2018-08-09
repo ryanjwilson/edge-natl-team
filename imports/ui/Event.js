8d603dd15ffea299e3b7392b781328a23526cc09
Ryan Wilson <xryanwilson@gmail.com>
2018-08-09 13:29:48 -0400

    disable body scroll while roster modal is open on mobile devices

--- imports/ui/Event.js
+++ imports/ui/Event.js
@@ -35,13 +35,13 @@ export class Event extends React.Component {
   showRosterModal() {
     Session.set('isRosterOpen', true);
     this.setState({ isRosterModalOpen: true });
-    disableBodyScroll(document.querySelector('.boxed-view__box-content'));
+    disableBodyScroll(document.querySelector('.boxed-view__modal'));
   }
 
   closeRosterModal() {
     Session.set('isRosterOpen', false);
     this.setState({ isRosterModalOpen: false });
-    enableBodyScroll(document.querySelector('.boxed-view__box-content'));
+    enableBodyScroll(document.querySelector('.boxed-view__modal'));
   }
 
   render() {
