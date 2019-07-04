245b2f0a32b4c1461c3827459aaa0a9ae6a40295
Ryan Wilson <xryanwilson@gmail.com>
2019-07-03 23:58:09 -0400

    refactored schema startup configuration

--- imports/startup/simple-schema-config.js
+++ imports/startup/simple-schema-config.js
@@ -1,5 +1,9 @@
-import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
+import { Meteor } from "meteor/meteor";
+import SimpleSchema from "simpl-schema";
+
+/**
+ * Defines a default schema validation error.
+ */
 
 SimpleSchema.defineValidationErrorTransform(function (error) {
 	return new Meteor.Error(400, error.message);
