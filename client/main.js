001364edae0e0f8d1bf4cca6fabedcb154a9b2fc
Ryan Wilson <xryanwilson@gmail.com>
2019-07-03 23:32:56 -0400

    refactored main client and server files

--- client/main.js
+++ client/main.js
@@ -1,36 +1,36 @@
-import { browserHistory } from 'react-router';
-import { Meteor } from 'meteor/meteor';
-import ReactDOM from 'react-dom';
-import { Session } from 'meteor/session';
-import { Tracker } from 'meteor/tracker';
+import { Meteor } from "meteor/meteor";
+import { browserHistory } from "react-router";
+import ReactDOM from "react-dom";
+import { Session } from "meteor/session";
+import { Tracker } from "meteor/tracker";
 
-import '../imports/startup/simple-schema-config.js';
-import { onAuthenticationChange, routes } from '../imports/routes/routes';
+import { onAuthenticationChange, routes } from "../imports/routes/routes";
+import "../imports/startup/simple-schema-config.js";
 
 /**
- * Tracks authentication changes via the isAuthenticated variable. This code is executed whenever a change is detected.
+ * Tracks changes in authentication status, and responds accordingly.
  */
 
 Tracker.autorun(() => {
 	const isAuthenticated = !!Meteor.userId();
-	const currentPagePrivacy = Session.get('currentPagePrivacy');
+	const currentPagePrivacy = Session.get("currentPagePrivacy");
 
 	onAuthenticationChange(isAuthenticated, currentPagePrivacy);
 });
 
 /**
- * Tracks changes in the selected Tournament or Wrestler Session variable. This code is executed whenever a change is detected.
+ * Tracks changes in tournament, wrestler, or team selection status, and responds accordingly.
  */
 
 Tracker.autorun(() => {
-	const selectedTournamentId = Session.get('selectedTournamentId');
-	const selectedWrestlerId = Session.get('selectedWrestlerId');
-	const selectedTeamId = Session.get('selectedTeamId');
+	const selectedTournamentId = Session.get("selectedTournamentId");
+	const selectedWrestlerId = Session.get("selectedWrestlerId");
+	const selectedTeamId = Session.get("selectedTeamId");
 
-	Session.set('isNavigationOpen', false);       // prevents admin sidebar panel animation on browser refresh
-	Session.set('isMenuOpen', false);             // prevents admin menu animation on browser refresh
-	Session.set('isApplicationOpen', false);      // prevents application animation on browser refresh
-	Session.set('isRosterOpen', false);           // prevents roster animation on browser refresh
+	Session.set("isNavigationOpen", false);       // prevents admin sidebar panel animation on browser refresh
+	Session.set("isMenuOpen", false);             // prevents admin menu animation on browser refresh
+	Session.set("isApplicationOpen", false);      // prevents application animation on browser refresh
+	Session.set("isRosterOpen", false);           // prevents roster animation on browser refresh
 
 	// conditionally re-direct the application based on selected tournament, wrestler, team, or current pathname.
 
@@ -41,85 +41,86 @@ Tracker.autorun(() => {
 	} else if (selectedTeamId) {
 		browserHistory.replace(`/teams/${selectedTeamId}`);
 	} else {
-		if (this.location.pathname.includes('/tournaments')) {
-			browserHistory.replace('/tournaments');
-		} else if (this.location.pathname.includes('/wrestlers')) {
-			browserHistory.replace('/wrestlers');
-		} else if (this.location.pathname.includes('/teams')) {
-			browserHistory.replace('/teams');
+		if (this.location.pathname.includes("/tournaments")) {
+			browserHistory.replace("/tournaments");
+		} else if (this.location.pathname.includes("/wrestlers")) {
+			browserHistory.replace("/wrestlers");
+		} else if (this.location.pathname.includes("/teams")) {
+			browserHistory.replace("/teams");
 		}
 	}
 });
 
-/**
- * Tracks changes in the sidebar menu status, allowing for the mobile-friendly slide-out item list panel to take effect.
- */
-
-// TODO - how will slide-out item list panel be handled for mobile version of the application?
-
-// Tracker.autorun(() => {
-//   document.body.classList.toggle('is-sidebar-open', Session.get('isSidebarOpen'));
-// });
-
 /*
- * Tracks changes in the mobile-frienly public navigation menu (i.e., opened or closed).
+ * Toggles the visibility (i.e., open or closed) of the mobile public navigation menu.
  */
 
 Tracker.autorun(() => {
-	document.body.classList.toggle('is-navigation-open', Session.get('isNavigationOpen'));
-	document.body.classList.toggle('no-scroll', Session.get('isNavigationOpen'));
+	document.body.classList.toggle("is-navigation-open", Session.get("isNavigationOpen"));
+	document.body.classList.toggle("no-scroll", Session.get("isNavigationOpen"));
 });
 
 /**
- * Tracks changes in the slide-out menu (desktop-only).
+ * Toggles the visibility (i.e., open or closed) of the desktop slide-out menu.
  */
 
 Tracker.autorun(() => {
-	document.body.classList.toggle('is-menu-open', Session.get('isMenuOpen'));
-	document.body.classList.toggle('no-scroll', Session.get('isMenuOpen'));
+	document.body.classList.toggle("is-menu-open", Session.get("isMenuOpen"));
+	document.body.classList.toggle("no-scroll", Session.get("isMenuOpen"));
 });
+
+/**
+ * Toggles the visibility of the roster modal.
+ */
 
 Tracker.autorun(() => {
-	document.body.classList.toggle('is-roster-open', Session.get('isRosterOpen'));
-	document.body.classList.toggle('no-scroll', Session.get('isRosterOpen'));
+	document.body.classList.toggle("is-roster-open", Session.get("isRosterOpen"));
+	document.body.classList.toggle("no-scroll", Session.get("isRosterOpen"));
 });
 
-/////// CLIENT APPLICATION EXECUTION STARTS HERE ///////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Executes when the application initially loads.
+ */
 
 Meteor.startup(() => {
-	Session.set('isNavigationOpen', false);       // public navigation menu (mobile-only)
-	Session.set('isMenuOpen', false);             // admin menu (mobile-only)
-	Session.set('isApplicationOpen', false);      // wrestler application (mobile-only)
-	Session.set('isRosterOpen', false);           // scheduled tournament list
+	Session.set("isNavigationOpen", false);       						// when the applicaiton first loads,
+	Session.set("isMenuOpen", false);             						// menus and modals (navigation, admin,
+	Session.set("isApplicationOpen", false);      						// wrestler application, and scheduled
+	Session.set("isRosterOpen", false);           						// tournament list) should be closed.
 
-	// setup selected tournament, wrestler, team, and message for each of the associated views
+	/*
+	 * Clear selections (and multiselections) for tournament, wrestler, team, and message views.
+	 */
 
-	Session.set('selectedTournamentId', undefined);
-	Session.set('selectedWrestlerId', undefined);
-	Session.set('selectedTeamId', undefined);
+	Session.set("selectedTournamentId", undefined);
+	Session.set("selectedWrestlerId", undefined);
+	Session.set("selectedTeamId", undefined);
+	//Session.set("selectedMessageId", undefined);
+	Session.set("multiselectedTournamentIds", []);
+	Session.set("multiselectedWrestlerIds", []);
+	Session.set("multiselectedTeamIds", []);
+	//Session.set("multiselectedMessageIds", []);
 
-	// setup multiselected tournaments, wrestlers, teams, and messages for each of the associated views
+	/*
+	 * Clear (or provide default values for) session variables that govern list filtering.
+	 */
 
-	Session.set('multiselectedTournamentIds', []);
-	Session.set('multiselectedWrestlerIds', []);
-	Session.set('multiselectedTeamIds', []);
+	Session.set("showPublishedFilter", true);                           // tournament view: events shown on public calendar
+	Session.set("showUnpublishedFilter", true);                         // tournament view: events hidden on public calendar
+	Session.set("selectedTournamentFilter", undefined);                 // wrestler view: tournament selected from dropdown menu
+	Session.set("selectedDivisionFilter", undefined);                   // wrestler view: division selected from dropdown menu
+	Session.set("selectedWeightClassFilter", undefined);                // wrestler view: weight class selected from dropdown menu
+	Session.set("selectedTeamTournamentFilter", undefined);             // team view, tournament selected from dropdown menu
+	Session.set("selectedTeamDivisionFilter", undefined);               // team view, division selected from dropdown menu
+	Session.set("selectedTeamTournamentModalFilter", undefined);        // team view, tournament selected from the modal dropdown menu
+	Session.set("selectedTeamDivisionNidakFilter", undefined);          // team view, division selected from the modal dropdown menu
 
-	// setup session variables for list filtering
+	/**
+	 * @todo add this to wrestler view, before team view?
+	 * Session.set("selectedWrestlerTournamentModalFilter", undefined);    // wrestler view, tournament selected from modal dropdown menu
+	 * Session.set("selectedWrestlerDivisionModalFilter", undefined);      // wrestler view, division selected from the modal dropdown menu
+	 * Session.set("selectedWrestlerWeightClassModalFilter", undefined);   // wrestler view, weight class selected from the modal dropdown menu
+	 */
 
-	Session.set('showPublishedFilter', true);                           // tournament view, events shown on public calendar
-	Session.set('showUnpublishedFilter', true);                         // tournament view, events hidden on public calendar
-
-	Session.set('selectedTournamentFilter', undefined);                 // wrestler view, tournament selected from dropdown menu
-	Session.set('selectedDivisionFilter', undefined);                   // wrestler view, division selected from dropdown menu
-	Session.set('selectedWeightClassFilter', undefined);                // wrestler view, weight class selected from dropdown menu
-	// Session.set('selectedWrestlerTournamentModalFilter', undefined);    // wrestler view, tournament selected from modal dropdown menu
-	// Session.set('selectedWrestlerDivisionModalFilter', undefined);      // wrestler view, division selected from the modal dropdown menu
-	// Session.set('selectedWrestlerWeightClassModalFilter', undefined);   // wrestler view, weight class selected from the modal dropdown menu
-
-	Session.set('selectedTeamTournamentFilter', undefined);             // team view, tournament selected from dropdown menu
-	Session.set('selectedTeamDivisionFilter', undefined);               // team view, division selected from dropdown menu
-	Session.set('selectedTeamTournamentModalFilter', undefined);        // team view, tournament selected from the modal dropdown menu
-	Session.set('selectedTeamDivisionNidakFilter', undefined);          // team view, division selected from the modal dropdown menu
-
-	ReactDOM.render(routes, document.getElementById('app'));
+	ReactDOM.render(routes, document.getElementById("app"));
 });
--- server/main.js
+++ server/main.js
@@ -1,16 +1,18 @@
-import { Accounts } from 'meteor/accounts-base';
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { Mongo } from "meteor/mongo";
 
-import '../imports/api/teams';
-import '../imports/api/tournaments';
-import '../imports/api/wrestlers';
-import '../imports/startup/simple-schema-config.js';
+import "../imports/api/teams";
+import "../imports/api/tournaments";
+import "../imports/api/wrestlers";
+import "../imports/startup/simple-schema-config.js";
 
-////////////////// SERVER APPLICATION EXECUTION STARTS HERE ////////////////////////////////////////////////////////////////////////////////
+/**
+ * Executes on application startup.
+ */
 
 Meteor.startup(() => {
 	if (Meteor.users.find().count() === 0) {
-		Accounts.createUser({ username: 'admin', password: 'admin' });
+		Accounts.createUser({ username: "admin", password: "admin" });
 	}
 });
