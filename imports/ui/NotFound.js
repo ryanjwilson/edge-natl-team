9fde840075b7c6989bbb3af98a43ddec1e61946b
Ryan Wilson <xryanwilson@gmail.com>
2019-07-08 01:39:57 -0400

    added custom 404 page

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,15 +1,34 @@
 import React from "react";
 
+import Header from "./Header";
+import Footer from "./Footer"
+
 /**
- * A component that renders a customer 404 page.
+ * A component that renders a custom 404 page.
  */
 
 export default class NotFound extends React.Component {
 	render() {
 		return (
 			<div>
-				NotFound component here.
+				<Header />
+
+				<div className="public-page-content">
+					<div className="public-page-content__not-found">
+						<div className="container">
+							<h5 className="container__title">404</h5>
+			
+							<div className="container__not-found">
+								<img src="/images/not-found.png" className="not-found__image" />
+								<p className="container__error-title">Oops! 404. That's an error.</p>
+								<p className="container__error-subtitle">The requested URL was not found.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<Footer />
 			</div>
-		)
+		);
 	}
 }
